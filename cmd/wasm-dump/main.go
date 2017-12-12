56cc9c8fa186eaf8912ff4f2cbb4861437b97c87
Sebastien Binet <binet@cern.ch>
2017-12-12 18:28:42 +0100

    cmd/wasm-dump: implement -x, -s and -d
    Examples:
    
      $> wasm-dump -s ./basic.wasm
      ./basic.wasm: module version: 0x1
    
      contents of section type:
      0000000a  01 60 00 01 7f                                    |.`...|
    
      contents of section function:
      00000011  01 00                                             |..|
    
      contents of section export:
      00000015  01 04 6d 61 69 6e 00 00                           |..main..|
    
      contents of section code:
      0000001f  01 05 00 41 2a 0f 0b                              |...A*..|
    
      $> wasm-dump -x ./basic.wasm
      ./basic.wasm: module version: 0x1
    
      section details:
    
      type:
       - type[0] <func [] -> [i32]>
      function:
       - func[0] sig=0
      export:
       - function[0] -> "main"
    
      $> wasm-dump -d ./basic.wasm
      ./basic.wasm: module version: 0x1
    
      code disassembly:
    
      func[0]: <func [] -> [i32]>
       000000: 41 2a 00 00 00             | i32.const 42
       000006: 0f                         | return
       000008: 0b                         | end

--- cmd/wasm-dump/main.go
+++ cmd/wasm-dump/main.go
@@ -5,30 +5,52 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"sort"
 
+	"github.com/go-interpreter/wagon/disasm"
 	"github.com/go-interpreter/wagon/wasm"
+	"github.com/go-interpreter/wagon/wasm/leb128"
+)
+
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `Usage: wasm-dump [options] file1.wasm [file2.wasm [...]]
+
+ex:
+ $> wasm-dump -h ./file1.wasm
+
+options:
+`,
+		)
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+}
+
+var (
+	flagVerbose = flag.Bool("v", false, "enable/disable verbose mode")
+	flagHeaders = flag.Bool("h", false, "print headers")
+	// flagSection = flag.String("j", "", "select just one section")
+	flagFull    = flag.Bool("s", false, "print raw section contents")
+	flagDis     = flag.Bool("d", false, "disassemble function bodies")
+	flagDetails = flag.Bool("x", false, "show section details")
 )
 
 func main() {
 	log.SetPrefix("wasm-dump: ")
 	log.SetFlags(0)
 
-	flagVerbose := flag.Bool("v", false, "enable/disable verbose mode")
-	flagHeaders := flag.Bool("h", false, "print headers")
-	// flagSection := flag.String("j", "", "select just one section")
-	flagFull := flag.Bool("s", false, "print raw section contents") // TODO(sbinet)
-	flagDis := flag.Bool("d", false, "disassemble function bodies") // TODO(sbinet)
-	flagDetails := flag.Bool("x", false, "show section details")    // TODO(sbinet)
-
 	flag.Parse()
 
 	if flag.NArg() < 1 {
 		flag.Usage()
-		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
@@ -41,7 +63,15 @@ func main() {
 
 	wasm.SetDebugMode(*flagVerbose)
 
-	fname := flag.Arg(0)
+	for i, fname := range flag.Args() {
+		if i > 0 {
+			fmt.Printf("\n")
+		}
+		process(fname)
+	}
+}
+
+func process(fname string) {
 	f, err := os.Open(fname)
 	if err != nil {
 		log.Fatalf("could not open %q: %v", fname, err)
@@ -55,6 +85,15 @@ func main() {
 
 	if *flagHeaders {
 		printHeaders(f.Name(), m)
+	}
+	if *flagFull {
+		printFull(f.Name(), m)
+	}
+	if *flagDis {
+		printDis(f.Name(), m)
+	}
+	if *flagDetails {
+		printDetails(f.Name(), m)
 	}
 }
 
@@ -139,5 +178,203 @@ func printHeaders(fname string, m *wasm.Module) {
 			sec.Start, sec.End, sec.PayloadLen,
 			sec.Name,
 		)
+	}
+}
+
+func printFull(fname string, m *wasm.Module) {
+	fmt.Printf("%s: module version: %#x\n\n", fname, m.Version)
+
+	hdrfmt := "contents of section %s:\n"
+	var sections []*wasm.Section
+
+	if sec := m.Types; sec != nil {
+		sections = append(sections, &sec.Section)
+	}
+	if sec := m.Import; sec != nil {
+		sections = append(sections, &sec.Section)
+	}
+	if sec := m.Function; sec != nil {
+		sections = append(sections, &sec.Section)
+	}
+	if sec := m.Table; sec != nil {
+		sections = append(sections, &sec.Section)
+	}
+	if sec := m.Memory; sec != nil {
+		sections = append(sections, &sec.Section)
+	}
+	if sec := m.Global; sec != nil {
+		sections = append(sections, &sec.Section)
+	}
+	if sec := m.Export; sec != nil {
+		sections = append(sections, &sec.Section)
+	}
+	if sec := m.Elements; sec != nil {
+		sections = append(sections, &sec.Section)
+	}
+	if sec := m.Code; sec != nil {
+		sections = append(sections, &sec.Section)
+	}
+	if sec := m.Data; sec != nil {
+		sections = append(sections, &sec.Section)
+	}
+	for i := range m.Other {
+		sections = append(sections, &m.Other[i])
+	}
+
+	for _, sec := range sections {
+		fmt.Printf(hdrfmt, sec.ID.String())
+		fmt.Println(hexDump(sec.Bytes, uint(sec.Start)))
+	}
+}
+
+func printDis(fname string, m *wasm.Module) {
+	fmt.Printf("%s: module version: %#x\n\n", fname, m.Version)
+	fmt.Printf("code disassembly:\n")
+	for i := range m.Function.Types {
+		f := m.GetFunction(i)
+		fmt.Printf("\nfunc[%d]: %v\n", i, f.Sig)
+		dis, err := disasm.Disassemble(*f, m)
+		if err != nil {
+			log.Fatal(err)
+		}
+		offset := 0
+		for _, code := range dis.Code {
+			n := 1
+			buf := new(bytes.Buffer)
+			str := new(bytes.Buffer)
+			fmt.Fprintf(buf, "%02x", code.Op.Code)
+			fmt.Fprintf(str, "%v", code.Op.Name)
+			for _, im := range code.Immediates {
+				imbuf := new(bytes.Buffer)
+				binary.Write(imbuf, binary.LittleEndian, im)
+				n += imbuf.Len() / 2
+				for _, cc := range imbuf.Bytes() {
+					fmt.Fprintf(buf, " %02x", cc)
+				}
+				fmt.Fprintf(str, " %v", im)
+			}
+			fmt.Printf(" %06x: %-26s | %s\n", offset, buf.String(), str.String())
+			offset += 2 * n
+		}
+		fmt.Printf(" %06x: %-26s | %s\n", offset, fmt.Sprintf("%02x", 0xb), "end")
+	}
+}
+
+func printDetails(fname string, m *wasm.Module) {
+	fmt.Printf("%s: module version: %#x\n\n", fname, m.Version)
+	fmt.Printf("section details:\n\n")
+
+	if sec := m.Types; sec != nil {
+		fmt.Printf("%v:\n", sec.ID)
+		for i, f := range sec.Entries {
+			fmt.Printf(" - type[%d] %v\n", i, f)
+		}
+	}
+	if sec := m.Import; sec != nil {
+		fmt.Printf("%v:\n", sec.ID)
+		for i, e := range sec.Entries {
+			buf := new(bytes.Buffer)
+			switch typ := e.Type.(type) {
+			case wasm.GlobalVarImport:
+				fmt.Fprintf(buf, "%s mutable=%v",
+					typ.Type.Type,
+					typ.Type.Mutable,
+				)
+			case wasm.FuncImport:
+				fmt.Fprintf(buf, "sig=%v", typ.Type)
+			case wasm.MemoryImport:
+				fmt.Fprintf(buf, "pages: initial=%d max=%d",
+					typ.Type.Limits.Initial,
+					typ.Type.Limits.Maximum,
+				)
+			case wasm.TableImport:
+				fmt.Fprintf(buf, "elem_type=%v init=%v max=%v",
+					typ.Type.ElementType,
+					typ.Type.Limits.Initial,
+					typ.Type.Limits.Maximum,
+				)
+			}
+			fmt.Printf(" - %v[%d] %s <- %s.%s\n",
+				e.Kind, i, buf.String(), e.ModuleName, e.FieldName,
+			)
+		}
+	}
+	if sec := m.Function; sec != nil {
+		fmt.Printf("%v:\n", sec.ID)
+		for i, t := range sec.Types {
+			fmt.Printf(" - func[%d] sig=%d\n", i, t)
+		}
+	}
+	if sec := m.Table; sec != nil {
+		fmt.Printf("%v:\n", sec.ID)
+		for i, e := range sec.Entries {
+			fmt.Printf(" - table[%d] type=%v initial=%v\n", i, e.ElementType, e.Limits.Initial)
+		}
+	}
+	if sec := m.Memory; sec != nil {
+		fmt.Printf("%v:\n", sec.ID)
+		for i, e := range sec.Entries {
+			fmt.Printf(" - memory[%d] pages: initial=%v\n", i, e.Limits.Initial)
+		}
+	}
+	if sec := m.Global; sec != nil {
+		fmt.Printf("%v:\n", sec.ID)
+		for i, g := range sec.Globals {
+			// TODO(sbinet) display init infos
+			fmt.Printf(" - global[%d] %v mutable=%v -- init: %#v\n", i, g.Type.Type, g.Type.Mutable, g.Init)
+		}
+	}
+	if sec := m.Export; sec != nil {
+		fmt.Printf("%v:\n", sec.ID)
+		keys := make([]string, 0, len(sec.Entries))
+		for n := range sec.Entries {
+			keys = append(keys, n)
+		}
+		sort.Strings(keys)
+		for _, name := range keys {
+			e := sec.Entries[name]
+			fmt.Printf(" - %v[%d] -> %q\n", e.Kind, e.Index, name)
+		}
+	}
+	if sec := m.Elements; sec != nil {
+		fmt.Printf("%v:\n", sec.ID)
+		for i, e := range sec.Entries {
+			fmt.Printf(" - segment[%d] table=%d\n", i, e.Index)
+			fmt.Printf(" - init: %#v\n", e.Offset)
+			for ii, elem := range e.Elems {
+				fmt.Printf("  - elem[%d] = func[%d]\n", ii, elem)
+			}
+		}
+	}
+	if sec := m.Data; sec != nil {
+		fmt.Printf("%v:\n", sec.ID)
+		for i, e := range sec.Entries {
+			fmt.Printf(" - segment[%d] size=%d - init %#v\n", i, len(e.Data), e.Offset)
+			fmt.Printf("%s", hexDump(e.Data, 0))
+		}
+	}
+	for _, sec := range m.Other {
+		fmt.Printf("%v:\n", sec.ID)
+		fmt.Printf(" - name: %q\n", sec.Name)
+		raw := bytes.NewReader(sec.Bytes[6:])
+		for {
+			if raw.Len() == 0 {
+				break
+			}
+			i, err := leb128.ReadVarUint32(raw)
+			if err != nil {
+				log.Fatal(err)
+			}
+			n, err := leb128.ReadVarUint32(raw)
+			if err != nil {
+				log.Fatal(err)
+			}
+			str := make([]byte, int(n))
+			_, err = io.ReadFull(raw, str)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf(" - func[%d] %v\n", i, string(str))
+		}
 	}
 }
